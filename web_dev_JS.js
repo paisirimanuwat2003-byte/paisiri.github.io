386946ed1b3fd08e44444282936a9e932ea39a1d
PaiSirimanuwat <161341701+PaiSirimanuwat@users.noreply.github.com>
2026-07-31 17:00:56 +0700

    recommit everything to avoid the file >100MB

--- web_dev_JS.js
+++ web_dev_JS.js
@@ -1,12 +1,16 @@
 // 1. Store the event data in an Array of Objects
 const eventsData = [
-  { year: "2014", title: "Introduction to Web Development", desc: "I started my journey in web development, in 2014, with adobe dreamweaver.", img: "./src/Dreamweaver.jpg" },
+  { year: "2014", title: "Introduction to Web Development", desc: "I started my journey in web development in 2014, with adobe dreamweaver, and, hosted my first website ever on 5GBfree.", img: "./src/Dreamweaver.jpg" },
   { year: "2015", title: "Learning the convenience of web builders", desc: "In 2015, I discovered web builders and began using platforms such as Weebly and Wix.", img: "./src/web_1.png" },
   { year: "2019", title: "Realizing the limitations of web builders", desc: "In 2019, I realized the limitations of web builders and decided to dive deeper into front-end web development.", img: "./src/vatska_2021.png" },
   { year: "2025", title: "Docker, Websocket and SQLite", desc: "In 2025, I had the opportunity to work with Docker, Websocket and SQLite.", img: "./src/TicTacToe.png" },
   { year: "2021-Present", title: "Renovation and Maintenance of Vatska.com", desc: "Since 2021-Present, I spearheaded the initial renovation and the ongoing maintenance of Vatska Company Limited's website.", img: "./src/vatska_com.png" },
 ];
   
+// 1.1 resizing the image to fit the container
+
+eventsData[0].style = "width: 100%; height: auto; object-fit: cover;";
+
 
 // 2. Select HTML elements
 const dots = document.querySelectorAll('.dot');
